71f4b04bbfb01928c89ada535acee17b5315e81b
Tote <tote@physics.rutgers.edu>
2015-07-24 15:11:48 -0500

    150724: A few changes as I figure out what sort of plotting pathway I'll have.

--- decortication/plots.py
+++ decortication/plots.py
@@ -1,9 +1,58 @@
 from truculence import *
 import samples
 import math
+from ROOT import TFile
 
-def make_th1():
+def make_th1(ntuple, max_events=-1):
 	info = {}
+	for collection in samples.collections:
+		info["pthat_{0}".format(collection)] = ["p_{T} Hat", "p_{T} hat (GeV)", 25, 0, 700]
+	
+	th1 = analysis.setup_th1(info)
+	
+	# Fill TH1s:
+	if max_events < 0:
+		max_events = ntuple.GetEntries()
+	n = -1
+	for event in ntuple:		# Loop over the ntuple.
+		n += 1
+		if n < max_events:
+			percent = n/float(utilities.roundup(max_events)) * 100
+			if percent % 10 == 0:
+				print "\t{0}%".format(int(percent))
+			for collection in samples.collections:
+				# Define variables:
+				try:		# The variable might not be in the event ...
+					# Kinematics:
+					pt0 = getattr(event, "{0}_pt".format(collection))[0]
+					pt1 = getattr(event, "{0}_pt".format(collection))[1]
+					pt_hat = getattr(event, "{0}_pt_hat".format(collection))[0]
+					m0 = getattr(event, "{0}_m".format(collection))[0]
+					m1 = getattr(event, "{0}_m".format(collection))[1]
+					m = (m0 + m1) / 2
+					dm = abs(m0 - m1)
+					eta0 = getattr(event, "{0}_eta".format(collection))[0]
+					eta1 = getattr(event, "{0}_eta".format(collection))[1]
+					phi0 = getattr(event, "{0}_phi".format(collection))[0]
+					phi1 = getattr(event, "{0}_phi".format(collection))[1]
+					dR = ((eta0 - eta1)**2 + (phi0 - phi1)**2)**(0.5)
+					
+					# Weighting:
+					sigma = getattr(event, "{0}_sigma".format(collection))[0]
+					nnorm = getattr(event, "{0}_nevents".format(collection))[0]
+					L = 10000		# The target luminosity in pb-1.
+					w = (sigma/nnorm)*L		# Calculate the event weight.
+#					w = 1
+				except Exception as ex:
+#					print ex
+					pass
+				else:
+					# Fill histograms:
+					## Fill single FJ plots:
+					th1["pthat_{0}".format(collection)].Fill(pt_hat, w)
+		else:
+			break
+	return th1
 
 def make_th2(ntuple, max_events=-1):
 	# Construct TH2s:
@@ -20,28 +69,28 @@ def make_th2(ntuple, max_events=-1):
 		info["pt0_pt1_{0}".format(collection)] = ["FJ Pt", "p_{T}^{0} (GeV)", "p_{T}^{1} (GeV)", 25, 0, 700, 25, 0, 700]
 		info["pt0_m0_{0}".format(collection)] = ["FJ Mass And Pt", "p_{T}^{0} (GeV)", "m^{0} (GeV)", 25, 0, 700, 25, 0, 700]
 		info["pt1_m1_{0}".format(collection)] = ["FJ Mass And Pt", "p_{T}^{1} (GeV)", "m^{1} (GeV)", 25, 0, 700, 25, 0, 700]
+		info["minpt_m_{0}".format(collection)] = ["#bar{m} Versus Min FJ Pt", "p_{T}^{min} (GeV)", "#bar{m} (GeV)", 25, 0, 700, 25, 0, 700]
 		info["m_dm_{0}".format(collection)] = ["FJ #Delta m Versus #bar{m}", "#bar{m} (GeV)", "#Delta m (GeV)", 50, 0, 500, 100, 0, 700]
 		info["logm_dm_{0}".format(collection)] = ["FJ #Delta m Versus #bar{m}", "Log(#bar{m})", "#Delta m (GeV)", 50, 0, 500, 70, 2.3, 6.6]
 	th2 = analysis.setup_th2(info)
 	
 	# Fill TH2s:
-	if max_events > 0:
-		n_events = max_events
-	else:
-		n_events = ntuple.GetEntries()
+	if max_events < 0:
+		max_events = ntuple.GetEntries()
 	n = -1
 	for event in ntuple:		# Loop over the ntuple.
 		n += 1
 		if n < max_events:
-			percent = n/float(n_events) * 100
+			percent = n/float(utilities.roundup(max_events)) * 100
 			if percent % 10 == 0:
-				print "{0}%".format(int(percent))
+				print "\t{0}%".format(int(percent))
 			for collection in samples.collections:
 				# Define variables:
 				try:		# The variable might not be in the event ...
 					# Kinematics:
 					pt0 = getattr(event, "{0}_pt".format(collection))[0]
 					pt1 = getattr(event, "{0}_pt".format(collection))[1]
+					minpt = min(pt0, pt1)
 					m0 = getattr(event, "{0}_m".format(collection))[0]
 					m1 = getattr(event, "{0}_m".format(collection))[1]
 					m = (m0 + m1) / 2
@@ -54,12 +103,12 @@ def make_th2(ntuple, max_events=-1):
 					
 					# Weighting:
 					sigma = getattr(event, "{0}_sigma".format(collection))[0]
-#					nnorm = getattr(event, "{0}_nevents".format(collection))[0]
-#					L = 10000		# The target luminosity in pb-1.
-#					w = (sigma/nnorm)*L		# Calculate the event weight.
-					w = 1
+					nnorm = getattr(event, "{0}_nevents".format(collection))[0]
+					L = 10000		# The target luminosity in pb-1.
+					w = (sigma/nnorm)*L		# Calculate the event weight.
+#					w = 1
 				except Exception as ex:
-					print ex
+#					print ex
 					pass
 				else:
 					# Fill histograms:
@@ -77,8 +126,47 @@ def make_th2(ntuple, max_events=-1):
 					th2["pt0_pt1_{0}".format(collection)].Fill(pt0, pt1, w)
 					th2["pt0_m0_{0}".format(collection)].Fill(pt0, m0, w)
 					th2["pt1_m1_{0}".format(collection)].Fill(pt1, m1, w)
+					th2["minpt_m_{0}".format(collection)].Fill(minpt, m, w)
 					th2["m_dm_{0}".format(collection)].Fill(m, dm, w)
 					th2["logm_dm_{0}".format(collection)].Fill(math.log(m), dm, w)
 		else:
 			break
 	return th2
+
+def get_th2(f=""):
+#	names = ["eta_phi", "pt_m", "eta_pt", "phi_pt", "m0_m1", "pt0_pt1", "pt0_m0", "pt1_m1", "minpt_m", "m_dm"]
+	
+	th2 = {}
+	
+	if "f":
+		if"/" in f:
+			tfile = TFile(f)
+		else:
+			tfile = TFile("{0}/{1}".format(samples.ntuple_location, f))
+		
+		tobjects = analysis.get_tobjects_all(tfile)
+		for tobject in tobjects:
+			th2[tobject.GetName()] = tobject
+		return th2
+	else:
+		print "You need to input a file name."
+		return th2
+
+def make_mass_distributions(th2):
+#	info = {}
+#	for collection in samples.collections:
+#		info["m_{0}".format(collection)] = ["Di-FJ Invariant Mass", "#bar{m} (GeV)", 25, 0, 700]
+#	th1 = analysis.setup_th1(info)
+	
+	tcanvas = analysis.setup_root()
+	
+	th1 = {}
+#	for i, th2 in enumerate(th2s):
+	for collection in samples.collections:
+		for x in range(25):
+			th1["m_{0}_cut{1}".format(collection, x)] = th2["minpt_m_{0}".format(collection)].ProjectionY("m_{0}_cut{1}".format(collection, x), x, 25, "e")
+	return th1
+#			if i == 0:
+#				th1d.Draw()
+#			else:
+#				
--- decortication/samples.py
+++ decortication/samples.py
@@ -1,17 +1,80 @@
 ntuple_location = "/eos/uscms/store/user/tote/data/fat"
 
+# Signals:
+sqtojjjj_info = {}
+lqtottau_info = {
+	"spring15": {
+		"LQLQToTopTau_M-200_TuneCUETP8M1_13TeV_pythia8": [
+			"/LQLQToTopTau_M-200_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM",
+			0.01132,
+			200,
+		],
+		"LQLQToTopTau_M-300_TuneCUETP8M1_13TeV_pythia8": [
+			"/LQLQToTopTau_M-300_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM",
+			0.3178,
+			300,
+		],
+		"LQLQToTopTau_M-500_TuneCUETP8M1_13TeV_pythia8": [
+			"/LQLQToTopTau_M-500_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM",
+			0.06163,
+			500,
+		],
+	},
+}
+
+# Backgrounds:
 qcd_info = {
 	"spring15": {
-		"QCD_Pt_50to80_TuneCUETP8M1_13TeV_pythia8": [19204300.0],
-		"QCD_Pt_80to120_TuneCUETP8M1_13TeV_pythia8": [2762530.0],
-		"QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8": [471100],
-		"QCD_Pt_170to300_TuneCUETP8M1_13TeV_pythia8": [117276],
-		"QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8": [7823],
-		"QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8": [648.2],
-#		"QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8": [186.9],
-		"QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8": [32.293],
-		"QCD_Pt_1000to1400_TuneCUETP8M1_13TeV_pythia8": [9.4183],
-		"QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8": [0.84265],
+		"QCD_Pt_50to80_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_50to80_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM",
+			19204300.0,
+			(50, 80),
+		],
+		"QCD_Pt_80to120_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_80to120_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM",
+			2762530.0,
+			(80, 120),
+		],
+		"QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_120to170_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM",
+			471100,
+			(120, 170),
+		],
+		"QCD_Pt_170to300_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_170to300_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM",
+			117276,
+			(170, 300),
+		],
+		"QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_300to470_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM",
+			7823,
+			(300, 470),
+		],
+		"QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_470to600_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM",
+			648.2,
+			(470, 600),
+		],
+		"QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_600to800_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM",
+			186.9,
+			(600, 800),
+		],
+		"QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_800to1000_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM",
+			32.293,
+			(800, 1000),
+		],
+		"QCD_Pt_1000to1400_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_1000to1400_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM",
+			9.4183,
+			(1000, 1400),
+		],
+		"QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8": [
+			"/QCD_Pt_1400to1800_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM",
+			0.84265,
+			(1400, 1800),
+		],
 	},
 	"phys14": {
 		"QCD_Pt-50to80_Tune4C_13TeV_pythia8": [22110000.0],
@@ -26,7 +89,24 @@ qcd_info = {
 		"QCD_Pt-1400to1800_Tune4C_13TeV_pythia8": [0.7346],
 	},
 }
+ttbar_info = {
+	"spring15": {
+		"TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8": [
+			"/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM",
+			670.3
+		],
+	},
+}
 
-samples = {j: k for key, value in qcd_info.iteritems() for j, k in value.iteritems()}		# Combined, single dictionary
+qcd_samples = {j: k for key, value in qcd_info.iteritems() for j, k in value.iteritems()}		# Combined, single dictionary
+ttbar_samples = {j: k for key, value in ttbar_info.iteritems() for j, k in value.iteritems()}
+lqtottau_samples = {j: k for key, value in lqtottau_info.iteritems() for j, k in value.iteritems()}
+samples = {}
+samples.update(qcd_samples)
+samples.update(ttbar_samples)
+samples.update(lqtottau_samples)
 
-collections = ["ak8_pf", "ca8_pf", "ca10_pf", "ca12_pf"]		# This should match the list that's in FatjetAnalyzer.cc
+collections = [		# This should match the list that's in FatjetAnalyzer.cc
+	"ak8_pf", "ca8_pf", "ak10_pf", "ca10_pf", "ak12_pf", "ca12_pf", "ak14_pf", "ca14_pf",
+	"ak8_gn", "ca8_gn", "ak10_gn", "ca10_gn", "ak12_gn", "ca12_gn", "ak14_gn", "ca14_gn",
+]
